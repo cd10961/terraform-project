3858429b08d4d0e2ada30ea51d5fc9c0d1f4a961
zhang Wei <136400441+cd10961@users.noreply.github.com>
2024-09-26 11:56:49 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,6 @@
 provider "aws" {
   region = "ap-northeast-1"
-  assume_role {
-    role_arn = var.aws_role_arn
-    web_identity_token = file(var.oidc_token_file)
 }
-}
-
-variable "aws_role_arn" {
-  description = "The ARN of the IAM role to assume"
-}
-
-variable "oidc_token_file" {
-  description = "The path to the OIDC token file"
-}
-
 resource "aws_s3_bucket" "ssd_bucket" {
   bucket = "ssd-trfm-bucket-test"
 
