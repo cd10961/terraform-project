0c6fd39851d973463e1ac1a9d58b0ad1adb5ba03
zhang Wei <136400441+cd10961@users.noreply.github.com>
2024-09-26 11:48:45 +0900

    main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ provider "aws" {
   region = "ap-northeast-1"
   assume_role {
     role_arn = var.aws_role_arn
-    web_identity_token = file(var.oidc_token_file)
+//    web_identity_token = file(var.oidc_token_file)
+    web_identity_token = var.oidc_token_file
 }
 
 variable "aws_role_arn" {
