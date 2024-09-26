43cb1bc69980d70efb35c318f81e61bcba2ddb4c
zhang Wei <136400441+cd10961@users.noreply.github.com>
2024-09-26 16:04:42 +0900

    main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,9 @@
 provider "aws" {
   region = "ap-northeast-1"
-
-  assume_role {
-    role_arn  = var.aws_role_arn
-  }
-}
-
-variable "aws_role_arn" {
-  description = "The ARN of the IAM role to assume"
 }
 
 resource "aws_s3_bucket" "ssd_bucket" {
-  bucket = "ssd-trfm-bucket-test"
+  bucket = "trfm-ssd-s3-bucket"
 
   versioning {
     enabled = true
