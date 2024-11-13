8562e3e24abcbc078b42e90a56512f117f6549e0
zhang Wei <136400441+cd10961@users.noreply.github.com>
2024-11-13 10:06:34 +0900

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -14,10 +14,10 @@ provider "aws" {
 #通常のS3バケット
 module "s3_buckets" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.1.1"
+  version = "2.8.0"
 
 #バケット名
-  bucket = "trfm-ssd-s3-test-000002"
+  bucket = "trfm-ssd-s3-test-000003"
 
 #バケット作成の要否
   create_bucket = true
