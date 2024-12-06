4daef20e8624f5ba97a029e09456498ad7dcdecb
zhang Wei <136400441+cd10961@users.noreply.github.com>
2024-12-06 10:03:02 +0900

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -20,7 +20,7 @@ module "s3_buckets" {
 #バケット名
   #bucket = "trfm-ssd-s3-test-000003"
   #bucket = "trfm-ssd-s3-test-000002"
-  bucket = "trfm-ssd-s3-test-000010"
+  bucket = "trfm-ssd-s3-test-000011"
 
 #バケット作成の要否
   create_bucket = true
